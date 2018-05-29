e189d4ef852ffd037a50b8eae57cb7e27e50c2d7
ZielMartin <Ziel.Martin@gmail.com>
2018-05-29 13:53:40 +0200

    Added OpenMesh to Windows MinGW.

--- include/HalfEdge.h
+++ include/HalfEdge.h
@@ -18,149 +18,26 @@
 #include <iostream>
 #include "NormalCalculation.h"
 
-
-
-#define VertPointer std::shared_ptr<HE_vert>
-#define EdgePointer std::shared_ptr<HE_edge>
-#define FacePointer std::shared_ptr<HE_face>
-
-#define VertList std::vector<VertPointer>
-#define EdgeList std::vector<EdgePointer>
-#define FaceList std::vector<FacePointer>
+#include "OpenMesh/Core/Mesh/PolyMesh_ArrayKernelT.hh"
 
 namespace cg {
-    struct HE_vert {
-        HE_vert(glm::vec3 pos) {
-            this->pos = glm::vec4(pos, 1.0);
-        }
-
-        HE_vert(glm::vec4 pos) {
-            this->pos = pos;
-        }
-
-        glm::vec4 pos;
-
-        bool selected = false;
-
-        glm::vec3 vertNormal;
-
-        /**
-         * one of the half-edges emanting from the vertex
-         */
-        std::shared_ptr<struct HE_edge> edge = nullptr;
+    struct MyTraits {
+        typedef glm::vec4 Point;
+        typedef glm::vec4 Normal;
+        typedef float Scalar;
     };
+    typedef OpenMesh::PolyMesh_ArrayKernelT<MyTraits> MyMesh;
 
-    struct HE_edge {
-        /**
-         * start-vertex of the half-edge
-         */
-        std::shared_ptr<struct HE_vert> vert = nullptr;
-
-        /**
-         * oppositely oriented adjacent half-edge
-         */
-        std::shared_ptr<struct HE_edge> pair = nullptr;
-
-        /**
-         * face the half-edge borders
-         */
-        std::shared_ptr<struct HE_face> face = nullptr;
-
-        /**
-         * next half-edge around the face
-         */
-        std::shared_ptr<struct HE_edge> next = nullptr;
-    };
-
-
-    struct HE_face {
-        /**
-         * one of the half-edges bordering the face
-         */
-        std::shared_ptr<struct HE_edge> edge = nullptr;
-
-        /**
-         * if true the Face is only a boundary for traversing, not a real face that should be rendered
-         */
-        bool isBoundary = false;
-
-        glm::vec3 faceNormal;
-
-    };
 
     class HE_Wrapper {
     public:
-        void addVert(VertPointer);
+        HE_Wrapper();
 
-        void addEdge(EdgePointer);
-
-        void addFace(FacePointer);
-
-        void addFaceVert(VertPointer vert);
-
-        VertPointer createVert(glm::vec4);
-
-        FacePointer createFace(VertList &);
-
-        void clear(void);
-
-        void deleteVert(VertPointer);
-
-        void deleteSelectedVertices() ;
-
-        void deleteEdge(EdgePointer);
-
-        void deleteFace(FacePointer);
-
-        void moveSelectedVertices(glm::vec3 relativeMovement);
-
-        VertPointer selectVertex(glm::vec3 worldCoordinates, float radius, bool markSelected);
-
-        void resetSelected();
-
-        const VertList &getVerts() const;
-
-        const VertList &getFaceVerts() const;
-
-        const EdgeList &getEdges() const;
-
-        const FaceList &getFaces() const;
-
-        std::map<std::shared_ptr<HE_vert>, std::vector<std::shared_ptr<HE_edge>>> &getAccelerationStruct();
-
+        void addVert(glm::vec4);
+        void createFace();
     private:
-        VertList verts;
-        EdgeList edges;
-        FaceList faces;
-        std::map<VertPointer, EdgeList > accelerationStruct;
-        VertList faceVerts;
+        MyMesh mesh;
 
-        EdgeList createEdgesFromVerts(const VertList &verts, FacePointer &newFace);
-
-        void checkOrientation(VertList &verts);
-
-        bool shouldReverse(const VertPointer &vert) const;
-
-        EdgePointer findPairAndSetIt(EdgePointer &currentEdge, EdgeList &outgoingEdgesFromDestination);
-
-        FacePointer findNeighborThatIsBoundary(const FacePointer face) const;
-
-        EdgePointer findEdgeConnectingFaces(const FacePointer faceA, const FacePointer faceB) const;
-
-        EdgePointer findPrevEdge(const std::shared_ptr<HE_edge> edge) const;
-
-
-        EdgeList getEdgesFromFace(FacePointer face);
-
-        EdgeList getEdgesFromVert(VertPointer vert);
-
-        void forEveryOutgoingEdgeFromVert(VertPointer vert, std::function<void(EdgePointer)> &func);
-
-        void forEveryEdgeAroundFace(FacePointer face, std::function<void(EdgePointer)> &func);
-
-        FaceList getFacesAroundVertex(VertPointer vertex);
-
-        std::map<FacePointer,EdgeList> getNeighborhood(VertPointer vert);
     };
 }
 #endif /* INCLUDE_HALFEDGE_H_ */
